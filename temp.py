2751ecb0924a29d26e90a4c7f671dc30ce2162f0
AbdelrahmanRadwan <abdelrahman.hamdy.radwan@gmail.com>
2018-04-11 17:17:24 +0200

    Updated the flow

--- temp.py
+++ temp.py
@@ -1,10 +1,11 @@
 import datetime
 from dateutil import parser
-a = "2017-08-01 00:06:47"
-b = "2017-08-02 00:31:05"
 
-a = parser.parse(a)
-b = parser.parse(b)
-c = b-a
-print(c.days)
+a = "2017-08-01 23:06:47"
 
+day = parser.parse(a)
+day = day.replace(hour=23)
+day = day.replace(minute=59)
+day = day.replace(second=59)
+
+print (day)
