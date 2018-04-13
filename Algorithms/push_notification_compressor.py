f67f81c4574c4d031fd635ecc7f2029eed37b1c7
AbdelrahmanRadwan <abdelrahman.hamdy.radwan@gmail.com>
2018-04-13 05:42:28 +0200

    fixed bugs in the algorithm

--- Algorithms/push_notification_compressor.py
+++ Algorithms/push_notification_compressor.py
@@ -4,6 +4,7 @@ import pandas as pd
 from tkinter import filedialog
 from tkinter import *
 from dateutil import parser
+from operator import itemgetter
 
 base_dir = dirname(dirname(abspath(__file__)))
 
@@ -22,8 +23,8 @@ class user:
     def __init__(self, id):
         self.id = id
         self.first_notification_time = 0
-        self.received_push_notifications = 0
-        self.number_of_notifications = 0
+        self.received_push_notifications = 0 # total number of notifications
+        self.number_of_notifications = 0 # per session
         self.last_notification_time = -1
         self.friends = list()
 
@@ -45,11 +46,13 @@ class user:
     '''
     def fire(self, time):
         global rows
-        message=""
+
+        if len(self.friends) < 1:
+            return
         if len(self.friends) == 1:
-            message += "%s went on a tour." %users_names[self.friends[0]]
-        elif len(self.friends) > 1:
-            message += "%s and %d other went on a tour." % (users_names[self.friends[0]], self.number_of_notifications - 1)
+            message = "%s went on a tour." %users_names[self.friends[0]]
+        else:
+            message = "%s and %d other went on a tour." % (users_names[self.friends[0]], len(self.friends) - 1)
 
         rows.append([time,
                      self.first_notification_time,
@@ -57,11 +60,15 @@ class user:
                      self.id,
                      message])
 
-        print(time, self.first_notification_time, self.number_of_notifications, self.id, message)
+        print(time,
+              self.first_notification_time,
+              self.number_of_notifications,
+              self.id,
+              message)
 
         self.received_push_notifications+=1
         self.friends.clear()
-        self.number_of_notifications = 0
+        self.number_of_notifications = 0 # per session
         self.last_notification_time = -1
         self.first_notification_time = 0
 
@@ -83,7 +90,7 @@ class user:
         time_difference = time_stamp - self.first_notification_time
         number_of_minutes = time_difference.seconds/60
         #the same day, and difference no more than 4 hours, and not the last notification in the day
-        if number_of_minutes <= 4*60 and self.received_push_notifications < 4 and time_difference.days==0:
+        if number_of_minutes <= 4*60 and self.received_push_notifications < 4:
             self.new_notification(time_stamp, friend_id)
         elif self.received_push_notifications == 4:
             self.new_notification(time_stamp, friend_id)
@@ -114,25 +121,31 @@ def get_directory():
 We started a new day or not
 '''
 def new_day(day1, day2):
-    days_difference = day2-day1
+    days_difference = day2 - day1
     # this should be a new day actually.
     # In this case we have to fire everything that we have from before.
     if days_difference.days:
         for _user in Users.values():
-            day1_temp = day1
-            if _user.received_push_notifications == 4:
-                day1_temp = day1_temp.replace(hour=23)
-                day1_temp = day1_temp.replace(minute=59)
-                day1_temp = day1_temp.replace(second=59)
-            else:
-                day_temp = day1_temp
+
+            day_temp = day1
+            day_temp = day_temp.replace(hour=23)
+            day_temp = day_temp.replace(minute=59)
+            day_temp = day_temp.replace(second=59)
+
+            day1_temp = _user.first_notification_time + datetime.timedelta(hours=4)
+            _user.fire(min(day_temp, day1_temp))
+
+    for _user in Users.values():
+        if _user.first_notification_time != 0:
+            days_difference = day2 - _user.first_notification_time
+            if days_difference.days:
+                day_temp = _user.first_notification_time
                 day_temp = day_temp.replace(hour=23)
                 day_temp = day_temp.replace(minute=59)
                 day_temp = day_temp.replace(second=59)
-                day1_temp = _user.first_notification_time + datetime.timedelta(hours=4)
-                day1_temp = min(day_temp, day1_temp)
 
-            _user.fire(day1_temp)
+                day1_temp = _user.first_notification_time + datetime.timedelta(hours=4)
+                _user.fire(min(day_temp, day1_temp))
 
 '''
 This is the last day in the sheet.
@@ -141,20 +154,14 @@ def end_days(day):
     # this should be a new day actually.
     # In this case we have to fire everything that we have from before.
     for _user in Users.values():
-        day_temp = day
-        if _user.received_push_notifications == 4:
+        if _user.number_of_notifications:
+            day_temp = day
             day_temp = day_temp.replace(hour=23)
             day_temp = day_temp.replace(minute=59)
             day_temp = day_temp.replace(second=59)
-        else:
-            day_temp2 = day_temp
-            day_temp2 = day_temp2.replace(hour=23)
-            day_temp2 = day_temp2.replace(minute=59)
-            day_temp2 = day_temp2.replace(second=59)
-            day_temp = _user.first_notification_time + datetime.timedelta(hours=4)
-            day_temp = min(day_temp, day_temp2)
 
-        _user.fire(day_temp)
+            day1_temp = _user.first_notification_time + datetime.timedelta(hours=4)
+            _user.fire(min(day_temp, day1_temp))
 
 def main():
 
@@ -173,7 +180,7 @@ def main():
         user_id = df["user_id"][i]
         friend_id = df["friend_id"][i]
         friend_name = df["friend_name"][i]
-       # A new friend, didn't apper before
+        # A new friend, didn't apper before
         if friend_id not in users_names:
             users_names[friend_id] = friend_name
         # A new user, didn't appear before
@@ -192,6 +199,7 @@ def main():
             end_days(time_stamp)
 
     # The push notifications output table
+    sorted(rows, key=itemgetter(2))
     df_output = pd.DataFrame(rows, columns=("notification_sent", "timestamp_first_tour", "tours", "receiver_id", "message"))
     df_output.to_csv(base_dir + "/Results/push_notifications.csv", encoding='utf-8', index=False)
 
